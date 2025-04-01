8a45f7b848aa2505dbc7da6a06449de39b859738
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-01 18:11:08 +0200

    fixup! python3Packages.zope-testing: 5.0.1 -> 5.1

--- pkgs/development/python-modules/zope-testing/default.nix
+++ pkgs/development/python-modules/zope-testing/default.nix
@@ -1,7 +1,7 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   isPyPy,
   setuptools,
   pytestCheckHook,
@@ -12,11 +12,17 @@ buildPythonPackage rec {
   version = "5.1";
   pyproject = true;
 
-  src = fetchPypi {
-    pname = "zope.testing";
-    inherit version;
-    hash = "sha256-cTXrquw9OHluSRqa2xJ6NlGqDjsgEo3mx3hFH6EazEc=";
+  src = fetchFromGitHub {
+    owner = "zopefoundation";
+    repo = "zope.testing";
+    tag = version;
+    hash = "sha256-G9RfRsXSzQ92HeBF5dRTI+1XEz1HM3DuB0ypZ61uHfw=";
   };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "setuptools <= 75.6.0" setuptools
+  '';
 
   build-system = [ setuptools ];
 
