64acb18cd81b762c2c35f46e1e95827f19811868
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-01 07:14:33 +0200

    python3Packages.pyopengl-accelerate: 3.1.7 -> 3.1.9
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pyopengl-accelerate/default.nix
+++ pkgs/development/python-modules/pyopengl-accelerate/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage rec {
   pname = "pyopengl-accelerate";
-  version = "3.1.7";
+  version = "3.1.9";
   format = "pyproject";
 
   src = fetchPypi {
     pname = "PyOpenGL-accelerate";
     inherit version;
-    hash = "sha256-KxI2ISc6k59/0uwidUHjmfm11OgV1prgvbG2xwopNoA=";
+    hash = "sha256-hZV8fHaXWBj/dZ7JJD+dxwke9vNz6jei61DDIP2ahvM=";
   };
 
   postPatch = ''
